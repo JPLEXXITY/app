00f988766461fd5b9cf4d6448c15a6c4e6d8faf2
JPLEXXITY <88306775+JPLEXXITY@users.noreply.github.com>
2024-11-14 18:35:46 +0300

    Added goods app, connected catalog & product html

--- main/views.py
+++ main/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 
 def index(request):
     context: dict[str, str] = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - Home',
-        'list': ['first', 'second', 'third', 'fourth'],
-        'dict': {'first': 1},
-        'bool': True,
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
     }
-    
+
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict[str, str] = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему этот магазин такой классный, и какой хороший товар.'
+    }
+
+    return render(request, 'main/about.html', context)
